7dea70ca140cc275ee87bacee2d748bed437c3fe
Trenton Parrotte <tparrotte@alarm.com>
2024-08-12 10:48:10 -0600

    Assignment 2: Added Optional number of pairs param

--- Memorize/Memorize/Models/Theme.swift
+++ Memorize/Memorize/Models/Theme.swift
@@ -10,12 +10,12 @@ import Foundation
 struct ThemeProvider {
     // To add a theme, create a new theme in the themes array
     static let themes: [MemoryGame<String>.Theme] = [
-        MemoryGame<String>.Theme(name: "Halloween", numberOfPairs: 5, emojis: ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"], color: "orange"),
-        MemoryGame<String>.Theme(name: "Animals", numberOfPairs: 6, emojis: ["🐶", "🐱", "🐭", "🐰", "🦊", "🐻", "🐼", "🐨", "🐯", "🦁", "🐮", "🐢"], color: "red"),
-        MemoryGame<String>.Theme(name: "Fruits", numberOfPairs: 7, emojis: ["🍎", "🍌", "🍇", "🍉", "🍓", "🍒", "🍑", "🍍", "🥝", "🍋", "🍊", "🫐"], color: "green"),
-        MemoryGame<String>.Theme(name: "Sports", numberOfPairs: 8, emojis: ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🏐", "🏉", "🎱", "🏓", "🏸", "🥏", "🏏"], color: "blue"),
-        MemoryGame<String>.Theme(name: "Faces", numberOfPairs: 9, emojis: ["😀", "😂", "😍", "😎", "🥳", "😡", "😭", "😴", "🤓", "😱", "🥸", "😤"], color: "yellow"),
-        MemoryGame<String>.Theme(name: "Flags", numberOfPairs: 10, emojis: ["🇺🇸", "🇬🇧", "🇨🇦", "🇫🇷", "🇩🇪", "🇮🇳", "🇯🇵", "🇨🇳", "🇧🇷", "🇮🇹", "🏴‍☠️", "🇦🇶"], color: "purple")
+        MemoryGame<String>.Theme(name: "Halloween", emojis: ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"], color: "orange", numberOfPairs: 5),
+        MemoryGame<String>.Theme(name: "Animals", emojis: ["🐶", "🐱", "🐭", "🐰", "🦊", "🐻", "🐼", "🐨", "🐯", "🦁", "🐮", "🐢"], color: "red", numberOfPairs: 6),
+        MemoryGame<String>.Theme(name: "Fruits", emojis: ["🍎", "🍌", "🍇", "🍉", "🍓", "🍒", "🍑", "🍍", "🥝", "🍋", "🍊", "🫐"], color: "green", numberOfPairs: 7),
+        MemoryGame<String>.Theme(name: "Sports", emojis: ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🏐", "🏉", "🎱", "🏓", "🏸", "🥏", "🏏"], color: "blue", numberOfPairs: Int.min),
+        MemoryGame<String>.Theme(name: "Faces", emojis: ["😀", "😂", "😍", "😎", "🥳", "😡", "😭", "😴", "🤓", "😱", "🥸", "😤"], color: "yellow", numberOfPairs: Int.max),
+        MemoryGame<String>.Theme(name: "Flags", emojis: ["🇺🇸", "🇬🇧", "🇨🇦", "🇫🇷", "🇩🇪", "🇮🇳", "🇯🇵", "🇨🇳", "🇧🇷", "🇮🇹", "🏴‍☠️", "🇦🇶"], color: "purple")
     ]
 }
 
@@ -26,9 +26,14 @@ extension MemoryGame {
         var emojis: [String]
         var color: String
         
-        init(name: String, numberOfPairs: Int, emojis: [String], color: String) {
+        init(name: String, emojis: [String], color: String, numberOfPairs: Int? = nil) {
             self.name = name
-            self.numberOfPairs = max(min(emojis.count, numberOfPairs), 4) // Ensure number of pairs is between 4 and emoji array size
+            if let numberOfPairs = numberOfPairs {
+                // Ensure number of pairs is between 4 and emoji array size
+                self.numberOfPairs = max(min(emojis.count, numberOfPairs), 4)
+            } else {
+                self.numberOfPairs = Int.random(in: 4...emojis.count)
+            }
             self.emojis = emojis
             self.color = color
         }
